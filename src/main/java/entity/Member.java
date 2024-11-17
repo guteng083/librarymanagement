dc21fff90ad292b39a0f342c71aeb71685b3ef71
Stefanus Arlian Situmeang <stevanusarlian@gmail.com>
2024-11-17 11:16:28 +0700

    [FEATURE] Add member entity

---
+++ src/main/java/entity/Member.java
@@ -0,0 +1,39 @@
+package entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+@Entity
+@Table(name = "member")
+public class Member {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+
+    @Column(nullable = false)
+    private String name;
+
+    private String email;
+
+    @Column(name = "phone_number")
+    private String phoneNumber;
+
+    @Column()
+    private int borrowedLimit = 3;
+
+    @Override
+    public String toString() {
+        return "Id = " + id + '\n' +
+                "Name = " + name + '\n' +
+                "Email = " + email + '\n' +
+                "Phone Number = " + phoneNumber + '\n' +
+                "Borrowed Limit = " + borrowedLimit;
+    }
+}
