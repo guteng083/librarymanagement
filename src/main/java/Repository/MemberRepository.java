871e57d1fad51c8abd7d738b10d56041acafea3b
Stefanus Arlian Situmeang <stevanusarlian@gmail.com>
2024-11-17 13:03:39 +0700

    [FEATURE] Add feature to add, view all, update, and delete member

---
+++ src/main/java/Repository/MemberRepository.java
@@ -0,0 +1,57 @@
+package Repository;
+
+import entity.Member;
+import jakarta.persistence.EntityManager;
+
+import java.util.List;
+
+public class MemberRepository {
+    private final EntityManager entityManager;
+
+    public MemberRepository(EntityManager entityManager) {
+        this.entityManager = entityManager;
+    }
+
+    public void save(Member member) {
+        try {
+            entityManager.getTransaction().begin();
+            entityManager.persist(member);
+            entityManager.getTransaction().commit();
+            System.out.println("Member saved successfully");
+        } catch (Exception e) {
+            if (entityManager.getTransaction().isActive()) {
+                entityManager.getTransaction().rollback();
+            }
+        }
+    }
+
+    public List<Member> findAll() {
+        return entityManager.createQuery("select m from Member m", Member.class).getResultList();
+    }
+
+    public void update(Member member) {
+        try{
+            entityManager.getTransaction().begin();
+            entityManager.merge(member);
+            entityManager.getTransaction().commit();
+            System.out.println("Member updated successfully");
+        } catch (Exception e) {
+            if (entityManager.getTransaction().isActive()) {
+                entityManager.getTransaction().rollback();
+            }
+        }
+    }
+
+    public void delete(Member member) {
+        try{
+            entityManager.getTransaction().begin();
+            entityManager.remove(member);
+            entityManager.getTransaction().commit();
+            System.out.println("Member deleted successfully");
+        } catch (Exception e) {
+            if (entityManager.getTransaction().isActive()) {
+                entityManager.getTransaction().rollback();
+            }
+        }
+    }
+}
